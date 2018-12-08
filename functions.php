052be9885014552d1c7c52b3ac14b6b73aa2d1f3
clive m <cmogusu@gmail.com>
2018-12-08 06:04:05 +0300

    a few more changes

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php
 add_action( 'wp_enqueue_scripts', 'wp_bootstrap_starter_child_enqueue_styles' );
 function wp_bootstrap_starter_child_enqueue_styles() {
-    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css',[],'0.2' ); 
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' ); 
 } 
 
 
